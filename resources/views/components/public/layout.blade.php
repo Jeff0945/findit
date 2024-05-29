da9a22b55d3111331b531bea0f63b847572129b9
Jeff Froilan Fortunado <jeff0945@gmail.com>
2024-05-29 23:52:49 +0800

    Add search and filter in admin and public

--- resources/views/components/public/layout.blade.php
+++ resources/views/components/public/layout.blade.php
@@ -9,6 +9,7 @@
 
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/font-awesome.min.css') }}">
 
     {{ $styles }}
 </head>
